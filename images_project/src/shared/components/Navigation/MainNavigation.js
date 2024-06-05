c3755845b5125157074ff0e763f36bc6590d75ce
Aparna venugopal <aparnavenugopal1996@gmail.com>
2024-06-05 23:05:19 +0530

    side bar

--- images_project/src/shared/components/Navigation/MainNavigation.js
+++ images_project/src/shared/components/Navigation/MainNavigation.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainNavigation.css';
 import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import NavLinks from './NavLinks';
+import SideDraw from './SideDraw';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  }
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  }
   return (
+    <>
+    {
+        drawerIsOpen && <Backdrop onClick={closeDrawer}/>
+    }
+     {
+     drawerIsOpen && (
+        <SideDraw>
+        <nav className="main-navigation__drawer-nav" onClick={openDrawer}>
+            <NavLinks/>
+        </nav>
+    </SideDraw>
+     ) 
+     }
+      
     <MainHeader>
         <button className="main-navigation__menu-btn">
-            <span></span>
-            <span></span>
-            <span></span>
+            <span />
+            <span />
+            <span />
         </button>
         <h1 className='main-navigation__title'>
             <Link to="/">YourPlaces</Link>
         </h1>
-        <nav>
-            ...
+        <nav className="main-navigation__header-nav">
+          <NavLinks/>
         </nav>
     </MainHeader>
+    </>
+   
   )
 }
 
