fc45e1f17246bbd9560cca22bf6d9434b8254fac
Aparna venugopal <aparnavenugopal1996@gmail.com>
2024-06-05 23:40:42 +0530

    side drawer done

--- images_project/src/shared/components/Navigation/MainNavigation.js
+++ images_project/src/shared/components/Navigation/MainNavigation.js
@@ -1,50 +1,47 @@
 import React, { useState } from 'react';
-import './MainNavigation.css';
-import MainHeader from './MainHeader';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import NavLinks from './NavLinks';
-import SideDraw from './SideDraw';
-import Backdrop from '../UIElements/Backdrop';
+import { Link } from 'react-router-dom';
 
-const MainNavigation = () => {
+import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
+import './MainNavigation.css';
+
+const MainNavigation = props => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-  const openDrawer = () => {
+
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true);
-  }
-  const closeDrawer = () => {
+  };
+
+  const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
-  }
+  };
+
   return (
-    <>
-    {
-        drawerIsOpen && <Backdrop onClick={closeDrawer}/>
-    }
-     {
-     drawerIsOpen && (
-        <SideDraw>
-        <nav className="main-navigation__drawer-nav" onClick={openDrawer}>
-            <NavLinks/>
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
         </nav>
-    </SideDraw>
-     ) 
-     }
-      
-    <MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span />
-            <span />
-            <span />
+      </SideDrawer>
+
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+          <span />
+          <span />
+          <span />
         </button>
-        <h1 className='main-navigation__title'>
-            <Link to="/">YourPlaces</Link>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
         </h1>
         <nav className="main-navigation__header-nav">
-          <NavLinks/>
+          <NavLinks />
         </nav>
-    </MainHeader>
-    </>
-   
-  )
-}
+      </MainHeader>
+    </React.Fragment>
+  );
+};
 
 export default MainNavigation;
--- images_project/src/user/pages/Users.js
+++ images_project/src/user/pages/Users.js
@@ -1,33 +1,19 @@
-import React from 'react'
+import React from 'react';
+
 import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
     {
-        id: 'u1',
-        name: 'krishna',
-        image: 'https://images.unsplash.com/photo-1712847332449-3791c8810949?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8',
-        places: 3,
-    },
-    {
-        id: 'u2',
-        name: 'aparna',
-        image: 'https://plus.unsplash.com/premium_photo-1714051661301-860c930a2c85?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8',
-        places: 1,
-    },
-    {
-        id: 'u3',
-        name: 'uday',
-        image: 'https://images.unsplash.com/photo-1717445956950-f12e558810a8?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8',
-        places: 5,
-    },
-    
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
   ];
-  return (
-    <div>
-        <UsersList items={USERS}/>
-    </div>
-  )
-}
+
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
